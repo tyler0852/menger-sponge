d3a359cac05f3dba2006b60799b169c0f8c7d65f
Tyler O'Brien <tylerobrien0852@gmail.com>
2026-02-26 19:12:48 -0600

    modifed to work with python 3.12+

--- make-menger.py
+++ make-menger.py
@@ -1,10 +1,11 @@
 import os
 import glob
-from distutils.dir_util import copy_tree
-import shutil
+##from distutils.dir_util import copy_tree # distutils was removed in Python 3.12, so we can use shutil.copytree instead
+from shutil import copytree
 
 srcfiles = glob.glob('./src/menger/*.ts')
 cmd = 'tsc --allowJs -m ES6 -t ES6 --outDir dist --sourceMap --alwaysStrict ' + " ".join(srcfiles) + ' ./src/lib/vue/vue.js'
 print('Building TypeScript: ' + cmd)
 os.system(cmd)
-copy_tree('./src/menger/static', './dist')
+##copytree('./src/menger/static', './dist')
+copytree('./src/menger/static', './dist', dirs_exist_ok=True)
